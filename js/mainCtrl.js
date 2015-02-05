73eb8101a772c0a9bc16d6c416d12024ac9d1298
mframpton21 <mike521@gmail.com>
2015-02-05 06:44:16 -0700

    checking in but need to get cookie store working

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,19 +1,32 @@
 var app = angular.module('quoteBook');
 
-app.controller('mainCtrl', function($scope, dataService){
+app.controller('mainCtrl', function($scope, dataService, $cookieStore){
 	$scope.test = "Connection good";
 
+
 	$scope.getData = function() {
-    	$scope.quotes = dataService.getData();
-  	}
+    $scope.quotes = $cookieStore.get('quotes');
 
-	$scope.addData = function(obj) {
-    	dataService.addData(obj);
-  	}
+  }$scope.addData = function(obj) {
+    $scope.quotes = $cookieStore.put('quotes', obj);
+  }
 
-  	$scope.removeData = function(string) {
-    	dataService.removeData(string);
-  	}
+  $scope.removeData = function(string) {
+    $scope.quotes = $cookieStore.remove('quotes');
+  }
 
-  	$scope.getData();
+  //$scope.getData();
+  $scope.quotes = $cookieStore.put('quotes', dataService.getData());
+
 });
+
+
+// angular.module('cookieStoreExample', ['ngCookies'])
+// .controller('ExampleController', ['$cookieStore', function($cookieStore) {
+//   // Put cookie
+//   $cookieStore.put('myFavorite','oatmeal');
+//   // Get cookie
+//   var favoriteCookie = $cookieStore.get('myFavorite');
+//   // Removing a cookie
+//   $cookieStore.remove('myFavorite');
+// }]);
